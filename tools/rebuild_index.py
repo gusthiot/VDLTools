4e2143e95269d5ca7db7801366889fd9d7c610a2
gusthiot <cgusthiot@gmail.com>
2019-11-21 17:56:18 +0100

    - rebuid on different thread

--- tools/rebuild_index.py
+++ tools/rebuild_index.py
@@ -21,8 +21,11 @@
  ***************************************************************************/
 """
 from future.builtins import object
+from .back_worker import BackWorker
 
-from PyQt4.QtCore import QCoreApplication
+from PyQt4.QtCore import QCoreApplication, Qt
+from PyQt4.QtGui import QProgressBar, QPushButton
+from qgis.gui import QgsMessageBar
 
 
 class RebuildIndex(object):
@@ -43,16 +46,31 @@ class RebuildIndex(object):
         """
         To start the rebuild
         """
-        snap_util = self.__iface.mapCanvas().snappingUtils()
-        extent = self.__iface.mapCanvas().extent()
-        # print(snap_util.indexingStrategy())
-        lcs_list = snap_util.layers()
-        for lc in lcs_list:
-            # print(lc.layer.name())
-            locator = snap_util.locatorForLayer(lc.layer)
-            locator.setExtent(extent)
-            # print(locator.hasIndex())
-            if not locator.hasIndex():
-                locator.init()
-            else:
-                locator.rebuildIndex()
+        self.__backWorker = BackWorker(self.__iface)
+        progressBar = QProgressBar()
+        progressBar.setAlignment(Qt.AlignLeft | Qt.AlignVCenter)
+        cancelButton = QPushButton()
+        cancelButton.setText('Cancel')
+        cancelButton.clicked.connect(self.__backWorker.kill)
+        self.__messageBar = self.__iface.messageBar().createMessage('Rebuild Index...', )
+        self.__messageBar.layout().addWidget(progressBar)
+        self.__messageBar.layout().addWidget(cancelButton)
+        self.__iface.messageBar().pushWidget(self.__messageBar, self.__iface.messageBar().INFO)
+
+        self.__backWorker.finishedSignal.connect(self.finished)
+        self.__backWorker.errorSignal.connect(self.error)
+        self.__backWorker.progressSignal.connect(progressBar.setValue)
+        self.__backWorker.start()
+
+    def finished(self):
+        self.__backWorker.quit()
+        self.__backWorker.wait()
+        self.__backWorker.deleteLater()
+        self.__iface.messageBar().popWidget(self.__messageBar)
+
+    def error(self, e, exception_string):
+        self.__iface.messageBar().pushMessage(
+            QCoreApplication.translate("VDLTools", "Worker thread raised an exception:\n") + format(exception_string),
+            level=QgsMessageBar.CRITICAL, duration=0)
+
+
